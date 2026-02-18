d9060414285f1712cd3eb0d1a3a2f0ac46e24076
unknown <anujtrikha199@gmail.com>
2026-02-18 23:31:35 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/random", (req, res) => {
+  res.json({
+    number: Math.floor(Math.random() * 1000),
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("ECS Fargate CI/CD Demo");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
